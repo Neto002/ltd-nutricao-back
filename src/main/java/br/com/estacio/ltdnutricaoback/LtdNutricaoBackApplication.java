ebf741333d395a0e7aadb6d1bf47ed75b444af25
Antonio Neto <antoniogfneto92@hotmail.com>
2024-04-09 17:28:43 -0300

    first commit

---
+++ src/main/java/br/com/estacio/ltdnutricaoback/LtdNutricaoBackApplication.java
@@ -0,0 +1,13 @@
+package br.com.estacio.ltdnutricaoback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LtdNutricaoBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LtdNutricaoBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/estacio/ltdnutricaoback/LtdNutricaoBackApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.estacio.ltdnutricaoback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LtdNutricaoBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
